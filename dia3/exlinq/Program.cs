35f9123727bd24b9a5473219cf78064c99b4020f
Deborah Regina Silveira <deborahregs@gmail.com>
2026-01-19 14:46:12 -0300

    Add files via upload

---
+++ dia3/exlinq/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exlinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Produto> produtos = new List<Produto>
+            
+            {new Produto("Celular",5000),
+            new Produto("Lápis",5),
+            new Produto("Mesa",1000),
+            new Produto("Relógio",45)};
+
+            List<Produto> produtos2 = new List<Produto>
+
+            {new Produto("Celular",5000),
+            new Produto("Lápis",5),
+            new Produto("Lápis",5),
+            new Produto("Lápis",5),
+            new Produto("Mesa",1000),
+            new Produto("Mesa",1000),
+            new Produto("Relógio",45)};
+
+            Action<List<Produto>> filtrar50 = list => list.
+            Where(produto => produto.Preco > 50)
+            .ToList().
+            OrderByDescending(produ => produ.Preco).
+            Reverse().
+            ToList().
+            ForEach(u => Console.WriteLine(u.Nome));
+
+            //filtrar50(produtos);
+
+            /*Lista de Vendas*/
+
+            Venda venda1 = new Venda(produtos);
+            Venda venda2 = new Venda(produtos2);
+
+            List<Venda> vendasProdutos = new List<Venda>();
+            vendasProdutos.Add(venda1);
+            vendasProdutos.Add(venda2);
+
+            Func<List<Venda>, double> totalVendas = vendas =>
+            {
+                var total = 0.0;
+                foreach(Venda vend in vendas)
+                {
+                    total = total + vend.CalculaTotal();
+                }
+
+            return total;
+            };
+
+            //
+            Func<List<Venda>, List<Tuple<string, double, double, double>> > mediaPorProduto = listaVendas =>
+            {
+
+                var list = listaVendas.Select(prop => prop.produtos).ToList();
+
+                List<Produto> listaTotal = new List<Produto>();
+                foreach (var prop in list)
+                {
+                
+                    foreach(Produto prod in prop)
+                    {
+                       listaTotal.Add(prod);
+                    }
+
+                }
+                    var resultado = listaTotal.GroupBy(pro => pro.Nome)
+                    .Select(g => new
+                    {
+                        Nome = g.FirstOrDefault().Nome,
+                        Quantidade = g.Count(),
+                        PrecoTotal = g.Sum(p => p.Preco),
+                        PrecoMedio = g.Average(p => p.Preco)
+                    });
+
+                    //resultado.ToList().ForEach(u => Console.WriteLine(u));
+
+                //var resultadoFinal = resultado.GroupBy(prodc => prodc.Nome).Select(g => new
+                //{
+                //    Nome = g.First().Nome,
+                //    Quantidade = g.Sum(p => p.Quantidade)
+                //});
+                //resultadoFinal.ToList().ForEach(u => Console.WriteLine(u));
+
+                List < Tuple<string, double, double, double> > listaFinal = new List<Tuple<string, double, double, double>>();
+                foreach (var result in resultado.ToList())
+                {
+                    var tupla = new Tuple<string,double, double, double> (result.Nome, result.Quantidade,result.PrecoTotal,result.PrecoMedio);
+                    listaFinal.Add(tupla);
+                }
+
+                return listaFinal;
+                
+            };
+
+            mediaPorProduto(vendasProdutos).ForEach(prod => Console.WriteLine(prod));
+        }
+    }
+
+        public class Produto
+        {
+            public string Nome;
+            public int Preco;
+
+            public Produto(string Nome, int Preco)
+            {
+                this.Nome = Nome;
+                this.Preco = Preco;
+            }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome + " Preço: " + Preco;
+        }
+    }
+
+        public class Venda
+        {
+            public List<Produto> produtos;
+
+            public Venda(List<Produto> produtos)
+            {
+                this.produtos = produtos;
+            }
+
+            public double CalculaTotal()
+            {
+                double soma = 0;
+
+                foreach(Produto prod in this.produtos)
+                {
+                    Console.WriteLine("Produto: "+ prod.Nome + " Valor: "+ prod.Preco);
+                    soma = soma + prod.Preco;
+                }
+
+                    Console.WriteLine("Total da Venda: "+ soma +"\n");
+                
+                return soma;
+            }
+        }
+
+    }
+
---
+++ dia3/lambda/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> ehPar = (int x) => (x % 2 == 0) ? true : false;
+
+            Console.WriteLine("Número é par? 2 - "+ ehPar(2));
+            Console.WriteLine("Número é par? 3 - "+ ehPar(3));
+
+            Action<int> calculaQuadrado = (int x) => Console.WriteLine("Quadrado de " + x + " é: " + Math.Pow(x, 2));
+
+            calculaQuadrado(2);
+            calculaQuadrado(7);
+
+            Func<string,string> converteMaiusculo = (string x) => x.ToUpper();
+
+            Console.WriteLine(converteMaiusculo("gato"));
+            Console.WriteLine(converteMaiusculo("cachorro"));
+
+            Func<List<int>,List<int>> filtrarPares = list =>
+            { 
+                Console.WriteLine("Lista Original:");
+                
+                list.Select((x,i) => new {Index = i, Value = x }).ToList().ForEach( u =>
+                {
+            
+                if(u.Index < list.Count - 1)
+                    {
+                        Console.Write(u.Value + ",");
+                    } else
+                    {
+                        Console.Write(u.Value + "\n");
+                    }
+                    
+                });
+                
+   
+                Console.WriteLine("\nLista Filtrada:");
+
+                foreach (var numero in list.Where(n => n % 2 == 0).Select(
+                    (x, i) => new { Index = i, Value = x }))
+                {
+                    if(numero.Index < list.Where(n => n%2 ==0).ToList().Count -1)
+                    {
+                        Console.Write(numero.Value + ",");
+                    } else
+                    {
+                        Console.Write(numero.Value + "\n");
+                    }
+                    
+                    
+                }
+                return list;
+            };
+
+            List<int> lista = filtrarPares(new List<int> { 1,2,3,4,5,6,7,8});
+
+            Func<List<int>, List<int>> OrdemCrescente = (numbers) => numbers.OrderBy(x => x).ToList();
+
+            List<int> lista1 = OrdemCrescente(new List<int> { 13, 21, 3, 445, 52, 61, 7, 18 });
+
+            lista1.Select((x, i) => new { Index = i, Value = x }).ToList().ForEach(u =>
+            {
+
+                if (u.Index < lista1.Count - 1)
+                {
+                    Console.Write(u.Value + ",");
+                }
+                else
+                {
+                    Console.Write(u.Value + "\n");
+                }
+
+            });
+
+            Func<List<int>, int> SomaMaior10 = numbers =>
+            {
+                int soma = 0;
+
+                numbers = numbers.Where(u => u>10).ToList();
+
+                numbers.ForEach(u => soma = soma + u);
+
+                numbers.Select((x, i) => new { Index = i, Value = x }).ToList().ForEach(u =>
+                {
+
+                    if (u.Index < numbers.Count - 1)
+                    {
+                        Console.Write(u.Value + ",");
+                    }
+                    else
+                    {
+                        Console.Write(u.Value + "\n");
+                    }
+
+                });
+                return soma;
+            };
+
+            Console.WriteLine(SomaMaior10(new List<int> { 1,2,3,10,20,30,50,100}));
+        }
+
+     
+    }
+
+
+
+
+}
